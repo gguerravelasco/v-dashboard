e88261121f8ca9764a5fdd4cc1457aacee811073
Galo Guerra <gguerravelasco@hotmail.com>
2024-11-26 17:19:55 -0500

    26112024

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="flex items-center justify-between px-6 py-4 bg-white border-b-4 border-indigo-600"
+    class="flex items-center justify-between px-6 py-4 bg-white border-b-4"
   >
     <div class="flex items-center">
       <button
